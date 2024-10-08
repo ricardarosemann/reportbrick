1a30bffb2892b214b58efb3adc896b0a7148b12b
Ricarda Rosemann <ricarda.rosemann@pik-potsdam.de>
2024-10-08 17:19:07 +0200

    Stud to implement price sensitivity normalization

---
+++ R/normalizePriceSensitivity.R
@@ -0,0 +1,9 @@
+#' Compute a normalized price sensitivity from life-cycle costs
+#'
+#' @param
+#'
+#' @author Ricarda Rosemann
+#'
+normalizePriceSensitivity <- function(dfLcc, brickRes, lambda) {
+
+}
